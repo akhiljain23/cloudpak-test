be04d7fdccea71a50b6492c55bab8d6d8c03f35b
akjain25 <akjain25@in.ibm.com>
2020-02-05 14:28:25 +0530

    add initial script

---
+++ cloudpak.py
@@ -0,0 +1,18 @@
+import requests 
+  
+# api-endpoint 
+URL = "http://127.0.0.1:5984/_all_dbs"
+  
+# location given here 
+location = "delhi technological university"
+  
+# defining a params dict for the parameters to be sent to the API 
+PARAMS = {'address':location} 
+  
+# sending get request and saving the response as response object 
+r = requests.get(url = URL, params = PARAMS) 
+  
+# extracting data in json format 
+data = r.json()
+
+print(data)
---
+++ cloudpak3.py
@@ -0,0 +1,74 @@
+import os
+import datetime
+from cloudant.client import Cloudant
+from cloudant import design_document
+from cloudant import database
+from cloudant import view
+
+# print(os.environ)
+startDate = os.environ['START_DATE']
+endDate = os.environ['END_DATE']
+catalogOfferingType = os.environ['CATALOG_OFFERING_TYPE']
+
+startDate = datetime.datetime.strptime(startDate, '%Y-%m-%d')
+endDate = datetime.datetime.strptime(endDate, '%Y-%m-%d')
+
+if endDate > startDate:
+    print(startDate, endDate, catalogOfferingType)
+
+client = Cloudant("admin", "admin", url="http://127.0.0.1:5984", connect=True)
+
+session = client.session()
+print('Username: {0}'.format(session['userCtx']['name']))
+print('Databases: {0}'.format(client.all_dbs()))
+#print(client.list_views())
+my_database1 = client["blueprint_db"]
+db_obj = database.CloudantDatabase(client, "blueprint_db")
+#print(db_obj.design_documents())
+print(db_obj.list_design_documents())
+
+ddoc = design_document.DesignDocument(db_obj, "_design/workspace")
+ddoc.fetch()
+#ddoc.add_view("sampleView", "")
+print(ddoc)
+print(ddoc.list_views())
+
+view_obj = view.View(ddoc, "workspacecountbyaccount")
+print(dir(view_obj))
+#print(view_obj.custom_result())
+#with view_obj.custom_result(include_docs=True) as rslt:
+#    for doc in rslt:
+#        print(doc)
+
+result = db_obj.get_view_result('_design/workspace', 'workspacecountbyaccount',
+    raw_result=True)
+print(result)
+
+res = db_obj.get_view_result('_design/workspace', 'workspacecountbyaccount',
+    raw_result=True, include_docs=True, skip=100, limit=100, reduce=False)
+res = db_obj.get_view_result('_design/workspace', 'workspacecountbyaccount',
+    raw_result=True, include_docs=True, limit=100, reduce=False)
+print(res)
+
+
+
+
+a = "1994-01-23"
+date_time_obj = datetime.datetime.strptime(a, '%Y-%m-%d')
+
+b = "2020-01-29T07:00:42+0000"
+date_time_obj2 = datetime.datetime.strptime(b, '%Y-%m-%dT%H:%M:%S+%f')
+
+all_dbs = client.all_dbs()
+docs = []
+for db in all_dbs:
+    if db == "blueprint_db":
+        my_database = client[db]
+        for document in my_database:
+            docs.append(document)
+print(len(docs))
+
+
+
+# Disconnect from the server
+client.disconnect()
