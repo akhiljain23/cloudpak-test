2f8456e39db184bf797c7a1680e9c94532592c63
Ravi Singh <ravsi117@in.ibm.com>
2020-02-12 12:13:32 +0530

    Added final changes

--- final.py
+++ final.py
@@ -22,33 +22,33 @@ def generate_records(start_date, end_date):
 
     final_res = []
     for row in res['rows']:
-        offering_installed_on = row['doc']['runtime_data'][0].get('created_on', '')
+        offering_installed_on = row['doc']['runtime_data'][0].get('created_on', 'Not Available')
         if offering_installed_on == "0001-01-01T00:00:00Z":
             offering_installed_on = ""
-        offering_destroyed_on = row['doc']['runtime_data'][0].get('destroyed_on', '')
+        offering_destroyed_on = row['doc']['runtime_data'][0].get('destroyed_on', 'Not Available')
         if offering_destroyed_on == "0001-01-01T00:00:00Z":
             offering_destroyed_on = ""
         cr_at = (row['doc']['created_at'])
         cr_at = datetime.datetime.strptime(cr_at[:10], '%Y-%m-%d')
         if cr_at >= start_date and cr_at <= end_date:
             entry = {
-                "account":row['doc'].get('account', ''),
-                "workspace_id":row['doc'].get('_id', ''),
-                "workspace_status":row['doc'].get('status', ''),
-                "workspace_region" : row['doc'].get('location', ''),
-                "workspace_createdDate": row['doc'].get('created_at', ''),
+                "account":row['doc'].get('account', 'Not Available'),
+                "workspace_id":row['doc'].get('_id', 'Not Available'),
+                "workspace_status":row['doc'].get('status', 'Not Available'),
+                "workspace_region" : row['doc'].get('location', 'Not Available'),
+                "workspace_createdDate": row['doc'].get('created_at', 'Not Available'),
                 "offering_installed_on" : offering_installed_on,
                 "offering_destroyed_on" : offering_destroyed_on,
-                "Offering_name" : row['doc']['catalog_ref'].get('item_name', ''),
-                "Offering_id" : row['doc']['catalog_ref'].get('item_id', ''),
-                "Offering_url" : row['doc']['catalog_ref'].get('item_url', ''),
-                "Offering_version" : row['doc']['catalog_ref'].get('offering_version', ''),
-                "cluster_id":row['doc']['shared_data'].get('cluster_id', ''),
-                "cluster_type" : row['doc']['shared_data'].get('cluster_type', ''),
-                "cluster_worker_count": row['doc']['shared_data'].get('worker_count', ''),
-                "cluster_worker_machine_type" : row['doc']['shared_data'].get('worker_machine_type', ''),
-                "cluster_created_on" : row['doc']['shared_data'].get('cluster_created_on', ''),
-                "cluster_region" : row['doc']['shared_data'].get('region', '')
+                "Offering_name" : row['doc']['catalog_ref'].get('item_name', 'Not Available'),
+                "Offering_id" : row['doc']['catalog_ref'].get('item_id', 'Not Available'),
+                "Offering_url" : row['doc']['catalog_ref'].get('item_url', 'Not Available'),
+                "Offering_version" : row['doc']['catalog_ref'].get('offering_version', 'Not Available'),
+                "cluster_id":row['doc']['shared_data'].get('cluster_id', 'Not Available'),
+                "cluster_type" : row['doc']['shared_data'].get('cluster_type', 'Not Available'),
+                "cluster_worker_count": row['doc']['shared_data'].get('worker_count', 'Not Available'),
+                "cluster_worker_machine_type" : row['doc']['shared_data'].get('worker_machine_type', 'Not Available'),
+                "cluster_created_on" : row['doc']['shared_data'].get('cluster_created_on', 'Not Available'),
+                "cluster_region" : row['doc']['shared_data'].get('region', 'Not Available')
             }
             final_res.append(entry)
     dataframe = pandas.DataFrame(final_res)
