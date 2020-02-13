a5811fcdbb2b2b9991c6d38d017bc14dfde9bdf5
akjain25 <akjain25@in.ibm.com>
2020-02-13 13:47:52 +0530

    Refactoring repo

---
+++ old_scripts/test1.py
@@ -0,0 +1,173 @@
+a = {
+  "_id": "plant-58469407-19bd-4e",
+  "_rev": "3-a88b55eceeb348098868673cfd904575",
+  "name": "plant",
+  "description": "",
+  "doc_type": "WORKSPACE",
+  "account": "12345678901234567890123456789012",
+  "owner": "schematics@ibm.com",
+  "resource_group": "",
+  "location": "US",
+  "secretid": "12345678901234567890123456789012",
+  "scope": "PRIVATEFORACCOUNT",
+  "type": [
+    "bash_script"
+  ],
+  "source_type": "BLUEPRINT",
+  "tags": [],
+  "created_at": "2020-02-11T18:33:33.63078+05:30",
+  "created_by": "schematics@ibm.com",
+  "deleted": "False",
+  "crn": "crn:v1:staging:public:schematics:us-south:a/0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0:00000000-0000-0000-0000-00000000000:workspace:plant-58469407-19bd-4e",
+  "status": "INPROGRESS",
+  "workspace_status": {
+    "frozen": "False",
+    "frozen_by": "",
+    "frozen_at": "0001-01-01T00:00:00Z",
+    "frozenreason": "",
+    "locked": "True",
+    "locked_by": "schematics@ibm.com",
+    "locked_time": "2020-02-11T18:33:33.661328+05:30"
+  },
+  "workspace_status_msg": {
+    "status_code": "",
+    "status_msg": ""
+  },
+  "internalvariables": [
+    {
+      "name": "$SCHEMATICS.SSHKEYPRIVATE",
+      "description": "",
+      "secure": "True",
+      "value": "$SCHEMATICSSECRET$01$.12345678901234567890123456789012.c4aea6a51d5a0b51a1ecec65.00d2207d059ceae2c8e2893c418efdb12a46808f28153851acfc2fd4449b48a54ca650c982601afcd8ac9aab5fbbb33297ad3c638c02fc8f504faa471fc199802d2d056d6ac7bde94e3872d18023c08e0cb8e8f83a2cea1f9561087f20b8f8046fe46c5a066d123035049e1c9af0631adda745391b0c3146914d039bdfb75571262b60d4a3c87626cdfb11a3a99170cc5ac7adfc09699f60219d1e4173d779b114426cd41e86c1b7fb4db3056eb0ba39a7bb63d8cf2a77b803c89bd10e7c6175754a6137fec2363709938270e6d9427d56e59dc0d3fff2f09e589b22b56426b3b5a7c750479588808b3790a8ca463b8705f7450d046d4e49d7f710206addc683f88e0ce3ad838cbcb2278cfc1c82ef325d2a0e86cadf90accbcbe858ccbd10ff65bb0f163ed7a2cf27d1f8ef1711f425a0ac93a76747ee9cbf4387495de603c0fda2aa3977253bc119fc595ac19c3adc7d28dbce240e2b6dca832747660e29cc049106fd15179e2f89b1a756ff6bbc48952ba085d873bb9a2e0ef0d793f16a99f176143616dd0dc326b4e735c627960d3c53c330294bd56ae72e11a008cee6bc19449957ab44593e7fcd6005b58cee85d069c9b857ba794715466a9f4a729f96ee076498043492a9acfbf2209e9cb30a7455b26fc0f2feb02a7ff104849ae40bd6df09984866e4eddb9894d0d0356fa5fa11f47d095c08e0bfa9cd1312386bce78df7ee2226af3e0215e6d778d47e7772eb42c7c44bf3996cb6bee7fd2d70b6e3b2cad9af8afabc75864578cd95dbf46466a441db302a4275da953466df0d11856915327c22910504fdd55d62da2888dbe3027f4cf8d43ac419c7cec626b47675eb66c6af723cd29a18370bb7a2d1ef980d5987dea7a66cf5fd4fcf9ee114ad25b6758220c7ee4267697fd084f851688e8c6ee7f09e909a4f7bd891ae340b6911962be4abfc1a01967c6c9f6ce76f181e3d77ecfa2bc5cbc53efcda00669fb19914784acd0cd73aa6539bd94383c2d93fd6d163d6794002cd795d3c708b87873067001c48a65232a4cf51ec59969390c071f41ec0eee67db28567582139083b2db9b39e8e87cc704d40bb18266a487a583b8441ab2127d093f21706e0f5134a3561f1e145e1712931d733ad6b5f3177beba51dd881bed5cc8bb1aa5069072cea66c26c9d6437e4fb3555cab2ee4937880c339ed8755b934694c64e807a6acabde370e9224c2e6abacf51e68d5694f352a301bf037f8fcd3c6fb6aa3d2b836b84be5cd1f7304afa902695a44739b55f6564cdc825032a5374cf961d2bed4411652e85813b71b2665da561297019cbd52df0d8a136568018e1c0a05d795c595bc3ce59cde8ea972ef1a4ed5c7d5b702eff9321e98046ae46a3e91e451c937f6d14961a14023e9aa6bbde831ec09c0d7e6b476f31219219b69cbd3240579060b2a3a55f06043d75d6eb7ab8b7a71a2658b9bb918e370192344014d07c8c0a4082f8607724a4174a54897db730f577cc24e0b64b85a99e82bebc637119f02ab3aa517a701068c8bcdc9bebac6960216de2a5dabbd56652a2113f137b8e5156452aa10dcc11d09aa661cf553c0ebbded9cdf23ecbefe94d901e0a2b536e9cb6cd75e184bce7b1f92d09b61423f42497c63b5c1d9daf6e1ac4fc5ef02a66d0df6b1168a1d4ea1bbf7d2060c39e0572f169c75ce94ef72f8da10d2966503c616cfbbd2a863722b9903ccf8752e830a7ce1918bc54f0035162af048e93180fbc3cdbda42f918195309c958361280d920ba6a6c15244dc4c74c8b2e3724cb027982594eccb029bc7b199d6acd953f0d245bc6748c683920f513558ec684b17615f826dacbefaafaf39502ba20e443f4a13b9ff5ed7b6cd2c94408ff8c70a380e748baec1a987591d476af3c64c52368a875653f4f22e21d90de73505682fd473bc5d828c8eac793e86b3dd289bf97e4cd5275ab710505b6476d154db2a033c7647efb44f3266bff1d15a332d3f119f7abbc6b004b0993b1425a1b9d305d74c273e8b9d0fc486c19ab25bf807c3ac6b6e9ed605696ed9d199f12b5a62617d3fa2e75a80daf5fea8d01415704e93650ff2aded39c004c665f2c5def33dca5de2da5357e329cd55f86f97f19cdd14cc5e40225d8c2fe840e89b64d1db1640f2dc7c6319c0857926284da98c45f082ac59b9a5d56dc2bb27001598485d2deee9bdfe82cdac829d5444a9d37b1dc15494b82b1d4328f7d9b4a59d21b939d5bc00520ef65f34b3e2c99e2383a1b754a17c96a13e30c1556cc149eca3b6d030aab59c5214998b4b163e59677274bf9937f77bf26d6479c38c1c2641694f352aab35e0da73e4be763578756749db09dc9db2472478eab6f41d6627f96b0fce3c5ad3fb8",
+      "type": ""
+    },
+    {
+      "name": "$SCHEMATICS.SSHKEYPUBLIC",
+      "description": "",
+      "secure": "False",
+      "value": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1ZCW/f0QL5C6AkaLQ4Ub0lk3FeyJP+JK50ajWLvFQUek5NaE2S623FgZtumSutF8w1ve5vh6YtRePi/+TT0xzOZKv9dBVESlT1QIJ35jyalGNUlgRoTlYOe4tY0SowU9qtnu/4JTRyRES9bVo3tKu2IQYSMl4+yiqyAduanT7aZrasuC3LhF07wCrQO9Pn6F+En+ffWsSN8lhB20OwaVwzFxH8/D0YNYbVB4lNKJhVsO4acq3vI/fPx9FMtgwiQNFxo2J0W9sQY2bZy2rQ+CMHf/Ootw4z4vUXn+mxF70x37bGXPR3H0m1t8H7oxfH+rh8sz88SW6iZO2hqhILvEN",
+      "type": ""
+    }
+  ],
+  "template_ref": "",
+  "catalog_ref": {
+    "item_id": "b636d651-8489-4425-bd6a-f30af1603577.ed142250-e49c-4e3b-8737-a68b638d5078",
+    "item_name": "Open Liberty",
+    "item_url": "content-mgmt/catalogs/b636d651-8489-4425-bd6a-f30af1603577/offering/367c3b09-f191-4e19-9eed-b3f5b2543254#about",
+    "item_readme_url": "http://localhost:8000/api/v1-beta/versions/b636d651-8489-4425-bd6a-f30af1603577.ed142250-e49c-4e3b-8737-a68b638d5078/about",
+    "launch_url": "www.google.com",
+    "item_icon_url": "www.someimageiconurl.com"
+  },
+  "template_repo": {
+    "url": "",
+    "repo_url": "https://github.ibm.com/juppad23/bash_script/archive/printenv2.tar.gz",
+    "release": ""
+  },
+  "template_data": [
+    {
+      "id": "bash_script-printenv2-797c2424-9dc6-46",
+      "folder": "bash_script-printenv2/bin/install_script.sh",
+      "uninstall_script_name": "",
+      "type": "bash_script",
+      "folder_key": "f65f00d8-2447-4db4-aa60-94edff9ae1f3",
+      "values_key": "9efb7d0e-7f64-4dde-9ef0-dd3391080972",
+      "values_url": "https://localhost:8080/v1/workspaces/plant-58469407-19bd-4e/template_data/bash_script-printenv2-797c2424-9dc6-46/values",
+      "values_metadata": [
+        {
+          "default": "noraml",
+          "description": "For testing a string type",
+          "env_var": True,
+          "hidden": False,
+          "immutable": False,
+          "label": "test normal",
+          "name": "TEST_NORMAL",
+          "options": "null",
+          "required": False,
+          "type": "string"
+        },
+        {
+          "default": "passssswaard",
+          "description": "To test a password type",
+          "env_var": False,
+          "hidden": False,
+          "immutable": False,
+          "label": "test password",
+          "name": "TEST_SECURE",
+          "options": "null",
+          "required": False,
+          "type": "password"
+        },
+        {
+          "default": "are not password hoooo",
+          "description": "To test a password type",
+          "env_var": True,
+          "hidden": False,
+          "immutable": False,
+          "label": "test password",
+          "name": "TEST_NOT_SECURE",
+          "options": "null",
+          "required": False,
+          "type": "string"
+        },
+        {
+          "default": "passssswaard",
+          "description": "To test a password type",
+          "env_var": True,
+          "hidden": False,
+          "immutable": False,
+          "label": "test password",
+          "name": "TEST_SECURE4",
+          "options": "null",
+          "required": False,
+          "type": "password"
+        },
+        {
+          "default": "passssswaard",
+          "description": "To test a password type",
+          "env_var": True,
+          "hidden": False,
+          "immutable": False,
+          "label": "test password",
+          "name": "TEST_SECURE3",
+          "options": "null",
+          "required": False,
+          "type": "password"
+        }
+      ],
+      "values": "",
+      "variablestore": "null",
+      "env_values": "null",
+      "state_key": "",
+      "log_key": "",
+      "env_key": "d3f63c9a-3493-4350-9f9f-37b450fb2d7a",
+      "sec_key": "65d5e57c-7d12-43bc-ada8-3737851e0110"
+    }
+  ],
+  "runtime_data": [
+    {
+      "id": "bash_script-printenv2-797c2424-9dc6-46",
+      "engine_name": "bash_script",
+      "engine_version": "",
+      "engine_cmd": "null",
+      "state_store_url": "https://localhost:8080/v1/workspaces/plant-58469407-19bd-4e/runtime_data/bash_script-printenv2-797c2424-9dc6-46/state_store",
+      "resources": "null",
+      "log_store_url": "https://localhost:8080/v1/workspaces/plant-58469407-19bd-4e/runtime_data/bash_script-printenv2-797c2424-9dc6-46/log_store",
+      "output_values": "null"
+    }
+  ],
+  "shared_data": {
+    "cluster_id": "bp14o4bd01gklvb3mqh0",
+    "namespace": "default",
+    "region": "us-south",
+    "resource_group_id": "55211bf37c9842a0957b460302f0ed84"
+  },
+  "updated_by": "",
+  "updated_at": "0001-01-01T00:00:00Z",
+  "vulnerability_factor": 0,
+  "git_token_key": "7c5697be-bc09-4702-bc6f-4afe635816d7",
+  "vulnerability_count": 0,
+  "last_health_check_at": "0001-01-01T00:00:00Z",
+  "resource_count": 0
+}
+
+print(a["runtime_data"][0].get("id", "0"))
